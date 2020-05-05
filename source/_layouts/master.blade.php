d66e1091826dda4d1b2795bda6102e0a7e5c6cdb
Lokman Musliu <lokman.musliu@gmail.com>
2020-05-06 00:51:42 +0200

    Added livechat feature

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -57,6 +57,21 @@
     </script>
     @endif
 
+    @if ($page->production)
+     <script type="text/javascript">
+         var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+         (function(){
+             var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+             s1.async=true;
+             s1.src='https://embed.tawk.to/5eb1e549a1bad90e54a20336/default';
+             s1.charset='UTF-8';
+             s1.setAttribute('crossorigin','*');
+             s0.parentNode.insertBefore(s1,s0);
+         })();
+     </script>
+    @endif
+
+
     @stack('scripts')
 </head>
 
